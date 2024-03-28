0049dfb110b490edcb9f754f043d685693bb9762
jchung4 <101676309+jchung4@users.noreply.github.com>
2024-03-28 16:51:48 -0400

    added tests

--- primes-service/src/test/java/edu/iu/jjchung/primesservice/PrimesServiceApplicationTests.java
+++ primes-service/src/test/java/edu/iu/jjchung/primesservice/PrimesServiceApplicationTests.java
@@ -6,8 +6,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PrimesServiceApplicationTests {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrimesServiceApplication.class, args);
-	}
+	class PrimesServiceTest {
+    PrimesService p = new PrimesService();
+
+    @Test
+    void _45isNotPrime() {
+        int n = 45;
+        boolean expected = false;
+        boolean actual = p.isPrime(n);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void _539828945930573IsNotPrime(){
+        long n = 539828945930573L;
+        boolean expected = false;
+        boolean actual = p.isPrime(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void _285191IsNotPrime(){
+        long n = 285191L;
+        boolean expected = true;
+        boolean actual = p.isPrime(n);
+        assertEquals(expected, actual);
+    }
 
 }
